51cb49c60d99bc9641deaf69e79f4246304a5d2e
liyca <66662110+liycabot@users.noreply.github.com>
2020-06-10 14:00:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,10 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print("むくっ")
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
